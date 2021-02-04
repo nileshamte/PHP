80bb68dec398732d77ed8019dc0fc04fafa42c49
nilesh <nilesha@eshopmarketing.in>
2021-02-04 10:57:14 +0530

    array

--- array.php
+++ array.php
@@ -1,25 +1,28 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 
-function arrayOpertion(){
-$arr = array(1, 2, 3, 4,5,6,7,8);
-$updatedArray = array();
-foreach ($arr as $value) {
-	
-    if($value==7)
-	$value=$value;
-	else
-    $value = $value * 2;
-    $updatedArray[]= $value;
-    
+function arrayOpertion()
+{
+    $arr = array(1, 2, 3, 4, 5, 6, 7, 8);
+    $updatedArray = array();
+    foreach ($arr as $value) {
+
+        if ($value == 7)
+            $value = $value;
+        else
+            $value = $value * 2;
+        $updatedArray[] = $value;
+    }
+    return $updatedArray;
+    //print_r(array_values($updatedArray));
 }
-return $updatedArray;
-//print_r(array_values($updatedArray));
-}print_r(array_values(arrayOpertion()));
+print_r(array_values(arrayOpertion()));
 
 ?>
-</body>
-</html>
+<!-- Given a following array
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+write a function which returns a new array having each value muliplied by 2 except 7. output:
+
+[2, 4, 6, 8, 10, 12, 7, 16]
+Advanced: Try to pass block to function as a argument which will handle the multiplication
+
+ -->
